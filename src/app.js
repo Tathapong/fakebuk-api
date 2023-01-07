4ce904b65a67162a81969b7367ffe56ab9c4c150
Tathapong <tathapong.gmail.com>
2023-01-07 12:32:37 +0700

    Initial setup api

---
+++ src/app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const notFoundMiddleware = require("./middlewares/notFound");
+const errorMiddleware = require("./middlewares/error");
+
+const app = express();
+
+//* Option tool (develop) for morgan
+// มันจะ log request http
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan());
+}
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("server running on port : " + port);
+});
